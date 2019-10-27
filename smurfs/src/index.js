c98bd7a301a2108ada7a43c08732ab5b6d4fb479
Kyle Leonard <kdleonard@icloud.com>
2019-10-27 18:29:40 -0500

    Started to work on reducer

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./components/App";
 
---
+++ smurfs/src/reducers/smurfReducer.js
@@ -0,0 +1,13 @@
+export const initialState = {
+    smurfs: []
+}
+
+
+export function reducer(state, action) {
+  switch (action.type) {
+    case: 
+
+    default:
+      return state
+  }
+}
