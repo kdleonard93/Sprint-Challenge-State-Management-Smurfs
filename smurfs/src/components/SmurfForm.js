86eadb600c95dacf42cc7d5c9b6e04d9ef0fae56
Kyle Leonard <kdleonard@icloud.com>
2019-10-27 19:06:28 -0500

    Set up state and created a Smurf Form component

---
+++ smurfs/src/components/SmurfForm.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function smurfForm() {
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="Name"></input>
+        <input type="text" placeholder="Age"></input>
+        <input type="text" placeholder="Height"></input>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default smurfForm;
