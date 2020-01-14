2288ee828c2fa0dfe958979c9200ca59f849c9e8
Kyle Leonard <kdleonard@icloud.com>
2020-01-13 18:26:36 -0600

    Re-worked Actions

--- smurfs/src/actions/smurfAction.js
+++ smurfs/src/actions/smurfAction.js
@@ -3,23 +3,19 @@ import axios from "axios";
 
 export const FETCH_SMURFS_NAME = "FETCH_SMURFS_NAME";
 export const FETCH_SMURFS_SUCCESS = "FETCH_SMURFS_SUCCESS";
-const POST_SMURF = "POST_SMURF";
+export const FETCH_SMURFS_FAIL = "FETCH_SMURFS_FAIL"
+export const POST_SMURF = "POST_SMURF";
 
-export const getSmurf = () => {
-  return dispatch => {
+export const getSmurf = () => dispatch => {
     dispatch({ type: FETCH_SMURF_NAME });
 
     axios
       .get("http://localhost:3333/smurfs")
-      .then(res => {
-        console.log(res.data);
-        dispatch({ type: FETCH_SMURF_SUCCESS, payload: res.data });
-      })
-      .catch(err => {
-        console.log("Error:", err);
-      });
+      .then(res => 
+        dispatch({ type: FETCH_SMURF_SUCCESS, payload: res.data })
+      )
+      .catch(err => dispatch ({ type: FETCH_SMURF_FAIL, payload: err}));
   };
-};
 
 export const postSmurf = newSmurf => {
   return dispatch => {
